2c2f46481578a1fbcc83ffe5ac11565107dc053b
MarcusJones <jones.0bj3@gmail.com>
2019-01-21 21:36:47 +0100

    update Loading benchmarks

--- src/manage/benchmark.py
+++ src/manage/benchmark.py
@@ -1,14 +1,39 @@
 import pandas as pd
 from pathlib import Path
+from sklearn.metrics import cohen_kappa_score
+import functools
+
+def kappa(y_true, y_pred):
+    return cohen_kappa_score(y_true, y_pred, weights='quadratic')
 
 ref_submissions = dict()
-ref_submissions['one'] = Path.cwd() / 'references' / 'Lukyanenko_Exploration_of_data_step_by_step' / 'submission.csv'
-ref_submissions['two'] = Path.cwd() / 'references' / 'Scuccimarra_PetFinder Simple LGBM Baseline' / 'submission.csv'
 
-for path_sub in ref_submissions:
-    this_path = ref_submissions[path_sub]
+name = 'Lukyanenko_Exploration_of_data_step_by_step'
+ref_submissions[name] = Path.cwd() / 'references' / name / 'submission.csv'
+
+name = 'Scuccimarra_PetFinder Simple LGBM Baseline'
+ref_submissions[name] = Path.cwd() / 'references' / name / 'submission.csv'
+
+dfs = dict()
+for ref_key in ref_submissions:
+    this_path = ref_submissions[ref_key]
     assert this_path.exists()
+    dfs[ref_key] = pd.read_csv(this_path)
+    dfs[ref_key].rename({'AdoptionSpeed':ref_key},inplace=True, axis='columns')
+#%%
+## df_final = functools.reduce(lambda left,right: pd.merge(left,right,on='PetID'), dfs)
+df_list = [dfs[ref_key] for ref_key in dfs]
 
-    df = pd.read_csv(this_path)
+df_final = df_list.pop(0)
+for df in df_list:
+    print(df.columns)
+    df_final = df_final.merge(df,on='PetID')
 
-
+# df_final = dfs['one'].merge(dfs['two'],on='PetID')
+df_final.set_index('PetID', drop=True, inplace=True)
+df_final.describe()
+# df_final.apply(pd.Series.value_counts, axis=1)
+# %%
+for col in df_final:
+    print(col)
+    print(df_final[col].value_counts())
