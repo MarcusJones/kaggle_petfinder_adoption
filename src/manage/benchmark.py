2b6fb4dafc49fc3b95c251e23da487fda756dedc
MarcusJones <jones.0bj3@gmail.com>
2019-01-22 20:35:42 +0100

    refactor rename, update benchmark

--- src/manage/benchmark.py
+++ src/manage/benchmark.py
@@ -3,6 +3,9 @@ from pathlib import Path
 from sklearn.metrics import cohen_kappa_score
 import functools
 from collections import OrderedDict
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
 def kappa(y_true, y_pred):
     return cohen_kappa_score(y_true, y_pred, weights='quadratic')
@@ -10,16 +13,18 @@ def kappa(y_true, y_pred):
 # Load the training data distribution
 
 
-
 #%%
 # Load reference kernel submissions
 ref_submissions = dict()
 
 name = 'Lukyanenko_Exploration_of_data_step_by_step 319'
-ref_submissions[name] = Path.cwd() / 'references' / name / 'submission.csv'
+ref_submissions[name] = Path.cwd() / 'reference_kernels' / name / 'submission.csv'
 
 name = 'Scuccimarra_PetFinder Simple LGBM Baseline 408'
-ref_submissions[name] = Path.cwd() / 'references' / name / 'submission.csv'
+ref_submissions[name] = Path.cwd() / 'reference_kernels' / name / 'submission.csv'
+
+name = 'current_kernel'
+ref_submissions[name] = Path.cwd() / 'kernel_submission' / 'submission.csv'
 
 dfs = dict()
 for ref_key in ref_submissions:
@@ -42,7 +47,7 @@ df_final.describe()
 # df_final.apply(pd.Series.value_counts, axis=1)
 # %%
 # count_df = pd.DataFrame()
-
+df_res = pd.DataFrame(index=range(5))
 for col in df_final:
     print(col)
     total = len(df_final[col])
@@ -55,8 +60,29 @@ for col in df_final:
 
     res = pd.concat([counts, percents], axis=1)
     res.sort_index(inplace=True)
-
+    df_res[col] = counts
     cnt_dict = counts.sort_index().to_dict()
-    print(cnt_dict)
+    # print(sum(cnt_dict.values()), cnt_dict)
 
+df_res.sum(axis=0)
+this_barchart = df_res.plot.bar()
+plt.show()
+
+#%%
+cnt_dict
+bar = plt.bar(range(len(cnt_dict)), cnt_dict.values())
+plt.xticks(range(len(cnt_dict)),)
+plt.show()
+fig, ax = plt.subplots()
+rects1 = ax.bar(ind - width/2, men_means, width, yerr=men_std,
+                color='SkyBlue', label='Men')
+rects2 = ax.bar(ind + width/2, women_means, width, yerr=women_std,
+                color='IndianRed', label='Women')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Scores')
+ax.set_title('Scores by group and gender')
+ax.set_xticks(ind)
+ax.set_xticklabels(('G1', 'G2', 'G3', 'G4', 'G5'))
+ax.legend()
 
