cab09a787ee769092a0b0773c09bbb7682021808
MarcusJones <jones.0bj3@gmail.com>
2019-01-28 19:10:49 +0100

    add reference and PandasSelector

--- src/0_env/environment_01_utilities.py
+++ src/0_env/environment_01_utilities.py
@@ -1,3 +1,56 @@
+# %%
+
+class PandasSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, columns=None, dtype=None, inverse=False,
+                 return_vector=True, name=None):
+        self.dtype = dtype
+        self.columns = columns
+        self.inverse = inverse
+        self.return_vector = return_vector
+        self.name = name
+
+        # Ensure columns in a list()
+        if isinstance(self.columns, str):
+            self.columns = [self.columns]
+
+        logging.info("Init {} on cols: {}".format(name, columns))
+
+    def check_condition(self, x, col):
+        cond = (self.dtype is not None and x[col].dtype == self.dtype) or \
+               (self.columns is not None and col in self.columns)
+        return self.inverse ^ cond
+
+    def fit(self, x, y=None):
+        return self
+
+    def _check_if_all_columns_present(self, x):
+        if not self.inverse and self.columns is not None:
+            missing_columns = set(self.columns) - set(x.columns)
+            if len(missing_columns) > 0:
+                missing_columns_ = ','.join(col for col in missing_columns)
+                raise KeyError(
+                    "Keys are missing in the record: {}, columns required:{}".format(missing_columns_, self.columns))
+
+    def transform(self, x):
+        logging.info("{} is transforming...".format(self.name))
+        # check if x is a pandas DataFrame
+        if not isinstance(x, pd.DataFrame):
+            raise KeyError("Input is not a pandas DataFrame it's a {}".format(type(x)))
+
+        selected_cols = []
+        for col in x.columns:
+            if self.check_condition(x, col):
+                selected_cols.append(col)
+
+        # if the column was selected and inversed = False make sure the column
+        # is in the DataFrame
+        self._check_if_all_columns_present(x)
+
+        # if only 1 column is returned return a vector instead of a dataframe
+        if len(selected_cols) == 1 and self.return_vector:
+            return list(x[selected_cols[0]])
+        else:
+            return x[selected_cols]
 
 #%%
 def timeit(method):
