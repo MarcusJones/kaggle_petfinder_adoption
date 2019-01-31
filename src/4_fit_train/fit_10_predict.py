4bb9afe399ad0138663cbdfaf90c98204fa27ee6
MarcusJones <jones.0bj3@gmail.com>
2019-01-31 08:13:59 +0100

    fix Still fails, check sorting of y

--- src/4_fit_train/fit_10_predict.py
+++ src/4_fit_train/fit_10_predict.py
@@ -19,7 +19,7 @@ sk.metrics.confusion_matrix(y_tr, y_tr_predicted)
 # NB we only want the defaulters column!
 predicted = clf_grid_BEST.predict(X_te)
 
-
+raise "Lost the sorting of y!"
 #%% Open the submission
 # with zipfile.ZipFile(path_data / "test.zip").open("sample_submission.csv") as f:
 #     df_submission = pd.read_csv(f, delimiter=',')
