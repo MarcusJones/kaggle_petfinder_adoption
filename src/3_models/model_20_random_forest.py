589c6c433294af76e8e2a58b92b41280a72c367e
MarcusJones <jones.0bj3@gmail.com>
2019-02-03 17:07:12 +0100

    udpate params

--- src/3_models/model_20_random_forest.py
+++ src/3_models/model_20_random_forest.py
@@ -14,24 +14,24 @@ clf = sk.ensemble.RandomForestClassifier(**params_model )
 
 #%% GridCV
 random_grid = {
-    'n_estimators': [int(x) for x in np.linspace(start = 800, stop = 1500, num = 8)],
+    'n_estimators': [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)],
     'max_features' : ['auto', 'sqrt'],
-    'max_depth' : [int(x) for x in np.linspace(10, 40, num = 8)] + [None],
+    'max_depth' : [int(x) for x in np.linspace(10, 110, num = 11)] + [None],
     'min_samples_split' : [2, 5, 10],
     'min_samples_leaf' : [1, 2, 4],
     'bootstrap' : [True, ],
-    # 'bootstrap' : [True, False],
+#     'bootstrap' : [True, False],
 }
 
 grid_lengths = [len(key) for key in random_grid.values()]
 grid_size = reduce(lambda x, y: x*y, grid_lengths)
 logging.info("Grid size {}".format(grid_size))
-# Best parameters: {'n_estimators': 1000, 'min_samples_split': 5, 'min_samples_leaf': 2, 'max_features': 'sqrt', 'max_depth': 20, 'bootstrap': True}
-# Best parameters: {'n_estimators': 400,  'min_samples_split': 2, 'min_samples_leaf': 2, 'max_features': 'auto', 'max_depth': 20, 'bootstrap': True}
-clf_grid = sk.model_selection.RandomizedSearchCV(estimator=clf, param_distributions=random_grid,
-                               n_iter=50, cv=3, verbose=50, random_state=42, n_jobs=-1)
 
-# clf_grid = sk.model_selection.GridSearchCV(clf, params_grid,
-#                                        verbose=1,
-#                                        cv=5,
-#                                        n_jobs=-1)
+N_ITER = 400
+CV_FOLDS = 5
+clf_grid = sk.model_selection.RandomizedSearchCV(estimator=clf, param_distributions=random_grid,
+                               n_iter=N_ITER, cv=CV_FOLDS, verbose=50, random_state=42, n_jobs=-1)
+
+
+logging.info("Total jobs: {}".format(N_ITER*CV_FOLDS))
+logging.info("Coverage: {:0.1%}".format((N_ITER)/grid_size))
--- src/4_fit_train/fit_00.py
+++ src/4_fit_train/fit_00.py
@@ -1,7 +1,8 @@
 #%% Fit
 if DEPLOYMENT != 'Kaggle':
+    import warnings
+    warnings.simplefilter("ignore", UserWarning)
 
-    print('asdf')
     clf_grid.fit(X_tr, y_tr)
 
     # Print the best parameters found
@@ -10,9 +11,12 @@ if DEPLOYMENT != 'Kaggle':
 
     clf_grid_BEST = clf_grid.best_estimator_
 else:
-    clf_grid_BEST = sk.ensemble.RandomForestClassifier(
-        {'n_estimators': 400, 'min_samples_split': 2, 'min_samples_leaf': 2, 'max_features': 'auto', 'max_depth': 20,
-         'bootstrap': True})
+
+    # params = {'n_estimators': 400, 'min_samples_split': 2, 'min_samples_leaf': 2, 'max_features': 'auto', 'max_depth': 20, 'bootstrap': True}
+    params = {'n_estimators': 400, 'min_samples_split': 5, 'min_samples_leaf': 2, 'max_features': 'auto', 'max_depth': 110, 'bootstrap': True}
+
+    clf_grid_BEST = sk.ensemble.RandomForestClassifier(**params)
+    clf_grid_BEST.fit(X_tr, y_tr)
 #%%
 
 
