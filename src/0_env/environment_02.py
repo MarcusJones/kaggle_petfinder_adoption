65816e23e98e3e54146b7159d3cab8682808e588
MarcusJones <jones.0bj3@gmail.com>
2019-03-10 08:51:15 +0100

    update Change to sklearn.model_selection.cross_val_score
    Needs .make_scorer!

--- src/0_env/environment_02.py
+++ src/0_env/environment_02.py
@@ -1,2 +1,49 @@
+if 0:
+    #%%
+    @depcretiated
+    def cross_validate(_X_tr, _y_tr, model, metric_function, folds=10, repeats=5):
+        """
+        Function to do the cross validation - using stacked Out of Bag method instead of averaging across folds.
+        model = algorithm to validate. Must be scikit learn or scikit-learn like API (Example xgboost XGBRegressor)
+        x = training data, numpy array
+        y = training labels, numpy array
+        folds = K, the number of folds to divide the data into
+        repeats = Number of times to repeat validation process for more confidence
+        :param _X_tr:
+        :param _y_tr:
+        :param model: SKLearn API Model
+        :param metric_function: takes y, y_pred
+        :param folds:
+        :param repeats:
+        :return:
+        """
+        y_cv_fold_pred = np.zeros((len(_y_tr), repeats))
+        score = np.zeros(repeats)
+        _X_tr = np.array(_X_tr)
+        for r in range(repeats):
+            i=0
+            logging.info("Cross Validating - Run {} of {}".format(str(r + 1), str(repeats)))
+            _X_tr, _y_tr = sk.utils.shuffle(_X_tr, _y_tr, random_state=r)    #shuffle data before each repeat
+            kf = sk.model_selection.KFold(n_splits=folds, random_state=i+1000)         #random split, different each time
+            for train_ind, cv_ind in kf.split(_X_tr):
+                print('Fold', i+1, 'out of', folds)
+                _X_tr_fold, _y_tr_fold = _X_tr[train_ind, :], _y_tr[train_ind]
+                _X_cv_fold, _y_cv_fold = _X_tr[cv_ind, :], _y_tr[cv_ind]
+                model.fit(_X_tr_fold, _y_tr_fold)
+                y_cv_fold_pred[cv_ind, r] = model.predict(_X_cv_fold)
+                i+=1
+            score[r] = metric_function(y_cv_fold_pred[:,r], _y_tr)
+
+        logging.info('\nCV complete, overall score: {}'.format(str(score)))
+        logging.info('Mean: {}'.format(str(np.mean(score))))
+        logging.info('Deviation: {}'.format(str(np.std(score))))
+
+    #%%
+    """
+    CV complete, overall score: [0.31460838 0.31986689 0.31859581 0.32246347 0.32236281]
+    2019-03-09 22:01:39 : Mean: 0.31957947269358394
+    2019-03-09 22:01:39 : Deviation: 0.002892279348388643
+    """
+    cross_validate(X_tr, y_tr, clf, kappa)
 
 
--- src/3_models/model_40_CV_Handler.py
+++ src/3_models/model_40_CV_Handler.py
@@ -7,44 +7,16 @@ X_tr = X_tr
 y_tr = y_tr
 X_te = X_te
 
-#%%
-def cross_validate(_X_tr, _y_tr, model, metric_function, folds=10, repeats=5):
-    """
-    Function to do the cross validation - using stacked Out of Bag method instead of averaging across folds.
-    model = algorithm to validate. Must be scikit learn or scikit-learn like API (Example xgboost XGBRegressor)
-    x = training data, numpy array
-    y = training labels, numpy array
-    folds = K, the number of folds to divide the data into
-    repeats = Number of times to repeat validation process for more confidence
-    :param _X_tr:
-    :param _y_tr:
-    :param model: SKLearn API Model
-    :param metric_function: takes y, y_pred
-    :param folds:
-    :param repeats:
-    :return:
-    """
-    y_cv_fold_pred = np.zeros((len(_y_tr), repeats))
-    score = np.zeros(repeats)
-    _X_tr = np.array(_X_tr)
-    for r in range(repeats):
-        i=0
-        logging.info("Cross Validating - Run {} of {}".format(str(r + 1), str(repeats)))
-        _X_tr, _y_tr = sk.utils.shuffle(_X_tr, _y_tr, random_state=r)    #shuffle data before each repeat
-        kf = sk.model_selection.KFold(n_splits=folds, random_state=i+1000)         #random split, different each time
-        for train_ind, cv_ind in kf.split(_X_tr):
-            print('Fold', i+1, 'out of', folds)
-            _X_tr_fold, _y_tr_fold = _X_tr[train_ind, :], _y_tr[train_ind]
-            _X_cv_fold, _y_cv_fold = _X_tr[cv_ind, :], _y_tr[cv_ind]
-            model.fit(_X_tr_fold, _y_tr_fold)
-            y_cv_fold_pred[cv_ind, r] = model.predict(_X_cv_fold)
-            i+=1
-        score[r] = metric_function(y_cv_fold_pred[:,r], _y_tr)
+scorer = sk.metrics.make_scorer(kappa, greater_is_better=True, needs_proba=False, needs_threshold=False)
 
-    logging.info('\nCV complete, overall score: {}'.format(str(score)))
-    logging.info('Mean: {}'.format(str(np.mean(score))))
-    logging.info('Deviation: {}'.format(str(np.std(score))))
+r = sk.model_selection.cross_val_score(clf, X_tr, y_tr, groups=None, scoring=scorer, cv=10, n_jobs=-1, verbose=8, fit_params=None, pre_dispatch='2*n_jobs', error_score='raise-deprecating')
+np.mean(r)
+r.mean()
 
-#%%
 
-cross_validate(X_tr, y_tr, clf, kappa)
+
+
+
+
+
+
